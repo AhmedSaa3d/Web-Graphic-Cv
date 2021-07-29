aea1cb2e68234ec44f09431c3864f674d92550c7
Ahmed Saad <67529228+AhmedSaa3d@users.noreply.github.com>
2021-07-29 03:03:28 +0200

    Add files via upload

---
+++ Web_Graphic C.V/js/Jquery.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+/******* The page scroll ****************/          
+$("html").niceScroll({
+            scrollspeed:"200",
+            cursorcolor:"#2ecc71"
+            });
+  /*********************************/
+ 
+      
+      
+      
+      
+      
+      
+      
+/********* Start features ***********************/
+   
+                                            
+/********* End features ***********************/
+       
+});
+  
+  
+  
+  
